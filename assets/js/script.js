284eb80414a4bb9630a40286f278e80b3a6d1e1f
Bell <jacobbrister71@gmail.com>
2022-07-03 21:10:11 -0400

    added basic home layout and question and answer arrays

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var questions = [
+    "What's the first answer?",
+    "What's the second answer?",
+    "What's the third answer?",
+    "What's the fourth answer?",
+    "What was the answer to the first question?"
+];
+
+var answers = [
+    [
+        "A", "A", "A", "A", "A"
+    ]
+    [
+        "B", "B", "B", "B", "B"
+    ]
+    [
+        "C", "C", "C", "C", "C"
+    ]
+    [
+        "D", "D", "D", "D", "D"
+    ]
+];
+
+var correctAnswers = [
+    "0", "1", "2", "3", "0"
+];
