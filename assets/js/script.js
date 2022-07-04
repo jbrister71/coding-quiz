addfe46dba26aef4d3381533fb5ae6019a51e3cd
Bell <jacobbrister71@gmail.com>
2022-07-03 22:49:14 -0400

    added questions that cycle until they run out

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,13 @@ var questions = [
 var answers = [
     [
         "A", "A", "A", "A", "A"
-    ]
+    ],
     [
         "B", "B", "B", "B", "B"
-    ]
+    ],
     [
         "C", "C", "C", "C", "C"
-    ]
+    ],
     [
         "D", "D", "D", "D", "D"
     ]
@@ -24,3 +24,57 @@ var answers = [
 var correctAnswers = [
     "0", "1", "2", "3", "0"
 ];
+
+startBtnEl = document.querySelector("#start-btn");
+answerContainerEl = document.getElementById("answers");
+var question = document.getElementById("questions");
+
+var questionCounter;
+
+var startQuiz = function() {
+    questionCounter = 0;
+    startBtnEl.remove();
+    document.getElementById("quiz-info").remove();
+    document.getElementById("quiz-info").remove();
+    console.log("Quiz started");
+    createQuestion();
+};
+
+var createQuestion = function() {
+    question.textContent = questions[questionCounter];
+    
+    var answerListEl = document.createElement("div");
+    answerListEl.className = "answer-list";
+
+    for(var i = 0; i < 4; i++) {
+        var answerBtnEl = document.createElement("button");
+        answerBtnEl.className = "answer-btn";
+        answerBtnEl.setAttribute("data-answer-id", i);
+        answerBtnEl.textContent = answers[i][questionCounter];
+        answerListEl.appendChild(answerBtnEl);
+    }
+    answerContainerEl.appendChild(answerListEl);
+};
+
+var checkAnswer = function(event) {
+    var answerId = event.target.getAttribute("data-answer-id");
+    if(answerId === correctAnswers[questionCounter]) {
+        console.log("Correct");
+        if(questionCounter < 4) {
+            questionCounter++;
+            document.querySelector(".answer-list").remove();
+            createQuestion();
+        }
+    }
+    else {
+        console.log("Incorrect");
+        if(questionCounter < 4) {
+            questionCounter++;
+            document.querySelector(".answer-list").remove();
+            createQuestion();
+        }
+    }
+}
+
+startBtnEl.addEventListener("click", startQuiz);
+answerContainerEl.addEventListener("click", checkAnswer);
