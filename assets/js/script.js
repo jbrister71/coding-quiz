87e0dd9cad436a21891ccb22a3a5ce1060d30a53
Bell <jacobbrister71@gmail.com>
2022-07-06 23:06:05 -0400

    added ability to save scores to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,17 @@ startContainerEl = document.querySelector("#start-container");
 startBtnEl = document.querySelector("#start-btn");
 answerContainerEl = document.getElementById("answers");
 var question = document.getElementById("questions");
+var formContainerEl;
+var formInputEl;
+var formInfoEl;
+var formButtonEl;
 
 var questionCounter;
 var timer = 0;
 numQuestions = 5;
 var interval;
 var clock = document.querySelector("#timer");
+var highScoreList = [];
 
 var startQuiz = function() {
     startTimer();
@@ -63,27 +68,35 @@ var createQuestion = function() {
 };
 
 var checkAnswer = function(event) {
-    var answerId = event.target.getAttribute("data-answer-id");
-    if(answerId === correctAnswers[questionCounter]) {
-        console.log("Correct");
-        timer += 10;
-        checkTimeUp();
-        displayAccuracy(true);
-        if(questionCounter < numQuestions-1) {
-            questionCounter++;
-            document.querySelector(".answer-list").remove();
-            createQuestion();
+    if(event.target.className === "answer-btn") {
+        var answerId = event.target.getAttribute("data-answer-id");
+        if(answerId === correctAnswers[questionCounter]) {
+            console.log("Correct");
+            timer += 10;
+            checkTimeUp();
+            displayAccuracy(true);
+            if(questionCounter < numQuestions-1 && timer > 0) {
+                questionCounter++;
+                document.querySelector(".answer-list").remove();
+                createQuestion();
+            }
+            else {
+                endQuiz();
+            }
         }
-    }
-    else {
-        console.log("Incorrect");
-        timer -= 20;
-        checkTimeUp();
-        displayAccuracy(false);
-        if(questionCounter < numQuestions-1) {
-            questionCounter++;
-            document.querySelector(".answer-list").remove();
-            createQuestion();
+        else {
+            console.log("Incorrect");
+            timer -= 20;
+            checkTimeUp();
+            displayAccuracy(false);
+            if(questionCounter < numQuestions-1 && timer > 0) {
+                questionCounter++;
+                document.querySelector(".answer-list").remove();
+                createQuestion();
+            }
+            else {
+                endQuiz();
+            }
         }
     }
 };
@@ -121,11 +134,66 @@ var checkTimeUp = function() {
         timer = 0;
         clock.textContent = "Time's up!";
         clearInterval(interval);
+        endQuiz();
     }
     else {
         clock.textContent = "Time: " + timer;
     }
+};
+
+var endQuiz = function() {
+    clearInterval(interval);
+    clock.textContent = "Time's up!";
+    question.textContent = "Let's see how you did!";
+    removeChildElements(answerContainerEl);
+    removeChildElements(startContainerEl);
+    
+    var result = document.createElement("p");
+    result.textContent = "Your score is " + timer + "!";
+    answerContainerEl.appendChild(result);
+
+    formContainerEl = document.createElement("div");
+    formInfoEl = document.createElement("p");
+    formInputEl = document.createElement("input")
+    formButtonEl = document.createElement("button");
+    formInputEl.className = "submit-input";
+    formButtonEl.className = "submit-btn";
+    formButtonEl.textContent = "Submit";
+
+    formInfoEl.textContent = "Enter your initials: ";
+    formContainerEl.appendChild(formInfoEl);
+    formContainerEl.appendChild(formInputEl);
+    formContainerEl.appendChild(formButtonEl);
+
+    answerContainerEl.appendChild(formContainerEl);
+};
+
+var saveHighScore = function(event) {
+    if(event.target.matches(".submit-btn")) {
+        console.log("we got here");
+        if(formInputEl.value) {
+            var highScoreObj = {
+                initials: document.querySelector(".submit-input").value,
+                score: timer
+            };
+            highScoreList.push(highScoreObj);
+            localStorage.setItem("scores", JSON.stringify(highScoreList));
+        }
+    }
+};
+
+var loadHighScores = function() {
+    var highScoreStr = localStorage.getItem("scores");
+    highScoreList = JSON.parse(highScoreStr);
 }
+
+var removeChildElements = function(element) {
+    while(element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+};
 
 startBtnEl.addEventListener("click", startQuiz);
 answerContainerEl.addEventListener("click", checkAnswer);
+answerContainerEl.addEventListener("click", saveHighScore);
+loadHighScores();
